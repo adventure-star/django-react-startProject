7244800931f5a6f6f29e3524f451e5e39ee14317
adventure-star <Boristolstyakov@protonmail.com>
2020-12-24 22:25:09 +0700

    frontend_app install, babel package install

--- sweetcode/sweetapp/models.py
+++ sweetcode/sweetapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post (models.Model):
    author = models.CharField(max_length=100)
    title = models.CharField(max_length=100)
-   content = models.CharField(max_length=100)
+   content = models.TextField(max_length=1000)
 
    def __str__(self):
        return self.title
